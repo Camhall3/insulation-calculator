99381ca43cc1d2f41515712f9edd5e92e583f69b
Camhall3 <cameron@ee-group.com.au>
2025-02-18 12:31:56 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import React from 'react';
 import InsulationBagLoadingForm from './components/InsulationBagLoadingForm';
 
 function App() {
+  // Get job_uuid from URL parameters (when opened from ServiceM8)
+  const urlParams = new URLSearchParams(window.location.search);
+  const jobUuid = urlParams.get('job_uuid');
+
   return (
     <div className="App">
-      <InsulationBagLoadingForm />
+      <InsulationBagLoadingForm jobUuid={jobUuid} />
     </div>
   );
 }
